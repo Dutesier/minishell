b10ff6c3316592e27412b1e7447bb6402bba635b
Duarte Areias dos Reis <duarte_reis_95@hotmail.com>
2021-11-19 15:25:54 +0000

    Minor changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dareias- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 14:51:49 by dareias-          #+#    #+#             */
-/*   Updated: 2021/11/18 20:09:43 by dareias-         ###   ########.fr       */
+/*   Updated: 2021/11/19 15:24:06 by dareias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,19 +43,18 @@ int main(int argc, char *argv[], char **envp)
 		if (shell.line != NULL)
 		{
 			parse_line(&shell);
-			i = 0;
 			if (shell.line[0] == 'q')
 				i = 0;
+			clean_shell(&shell);
 		}
 	}
-	int c = 0;
+	/*int c = 0;
 	while (shell.commands[c] != NULL)
 	{
 		printf("%s\n", shell.commands[c]->cmd);
 		c++;
-	}
+	}*/
 	clear_history();
-	clean_shell(&shell);
 	
 	return (0);
 }
